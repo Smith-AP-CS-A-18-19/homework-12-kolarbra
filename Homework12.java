e2a576f8b960d782e4d1e386895dfa153bcceb66
Kolar <kolarbra@berkeleyprep.org>
2019-01-28 12:27:54 -0500

    done

--- Homework12.java
+++ Homework12.java
@@ -7,18 +7,20 @@ import java.util.ArrayList;
 
 public class Homework12 {
 
+	ArrayList<RectangularShape> arr;
+
 	/* Finish the constructor and create any necessary instance
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
 	public Homework12() {
-
+		arr = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		arr.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +28,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+		int num = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i).intersects(r)) {
+				num++;
+			}
+		}
+		return num;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,21 +42,40 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int num = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (!arr.get(i).contains(p)) {
+				num++;
+			}
+		}
+		return num;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int num = 0;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) instanceof Ellipse2D) {
+				num++;
+			}
+		}
+		return num;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		for (RectangularShape shape : arr) {
+			if (shape instanceof Rectangle2D) {
+				if (l.intersects((Rectangle2D) shape)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +87,15 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double area = 0.0;
+ 		for (RectangularShape shape : arr) {
+			if (shape instanceof Rectangle2D) {
+				area += shape.getHeight() * shape.getWidth();
+			} else if (shape instanceof Ellipse2D) {
+				area += Math.PI * (shape.getWidth() / 2) * (shape.getHeight() / 2);
+			}
+		}
+		return area;
 	}
 
 	public static void main(String[] args) {
@@ -71,7 +106,7 @@ public class Homework12 {
 		hw12.addShape(new Rectangle2D.Double(30, 30, 20, 20));
 		hw12.addShape(new Rectangle2D.Double(40, 20, 20, 40));
 
-		Rectangle2D  r = new Rectangle2D.Double(25, 25, 10, 10);
+		Rectangle2D r = new Rectangle2D.Double(25, 25, 10, 10);
 		if (hw12.problem1(r) == 2) {
 			System.out.println("Pass 1");
 		} else {
